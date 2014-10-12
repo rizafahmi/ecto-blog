5e5e953da957d199256dc632cb9b02114ba46a61
Riza Fahmi <rizafahmi@gmail.com>
2014-10-12 19:41:13 +0700

    Add alter table migration script and run the migration

---
+++ priv/repo/migrations/20141012122520_alter_comments_table.exs
@@ -0,0 +1,18 @@
+defmodule EctoBlog.Repo.Migrations.AlterCommentsTable do
+  use Ecto.Migration
+
+  def up do
+    ["ALTER TABLE comments
+      ADD blogs_id INTEGER",
+      "ALTER TABLE comments
+      ADD FOREIGN KEY(blogs_id) REFERENCES blogs(id);"]
+  end
+
+  def down do
+    """
+    ALTER TABLE comments
+      DROP CONSTRAINT "comments_id_fkey",
+      DROP blogs_id
+    """
+  end
+end
